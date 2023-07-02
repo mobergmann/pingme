0d94f2b3c8b962d71ad73de25ed7c140c7447c7b
mobergmann <mobergmann@pm.me>
2023-07-02 23:31:20 +0200

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -20,39 +20,49 @@ int main(int argc, char **argv)
     bot.on_log(dpp::utility::cout_logger());
 
     bot.on_slashcommand([&bot](const dpp::slashcommand_t &event) {
-        if (event.command.get_command_name() == "newsletter") {
-            std::string channel = std::get<std::string>(event.get_parameter("channel"));
-            newsletter[event.command.user.id].insert(channel);
-            event.reply(fmt::format("You applied for the newsletter for the channel {}", channel));
-            // todo test if bot has access to channel
+        if (event.command.get_command_name() == "list") {
+            const auto& channels = newsletter[event.command.usr.id];
+            if (channels.empty()) {
+                event.reply("You have no active subscriptions.");
+            }
+            else {
+                std::string reply = "You subscribed for the newsletter of the following channels: ";
+                for (const auto& i: channels) {
+                    reply += "\n - " + std::to_string(i);
+                }
+                event.reply(reply);
+            }
         }
-        else if (event.command.get_command_name() == "list") {
-            event.reply(fmt::format("You applied for the newsletter of the following channels: {}", [event.command.user.id]));
+        else if (event.command.get_command_name() == "subscribe") {
+            dpp::snowflake channel = std::get<dpp::snowflake>(event.get_parameter("channel"));
+            event.reply(std::to_string(channel));
+//            // todo test if bot has access to channel
+//            newsletter[event.command.usr.id].insert(channel);
+//            event.reply(fmt::format("You subscribed for the newsletter for the channel {}", channel));
         }
         else if (event.command.get_command_name() == "unsubscribe") {
-            newsletter.remove(event.command.user.id);
+            dpp::snowflake channel_id = std::get<dpp::snowflake>(event.get_parameter("channel"));
+            newsletter[event.command.usr.id].erase(channel_id);
+            event.reply(fmt::format("You have successfully removed the channel {} from your newsletter", std::to_string(channel_id)));
         }
     });
 
     bot.on_ready([&bot](const dpp::ready_t &event) {
         if (dpp::run_once<struct register_bot_commands>()) {
-            dpp::slashcommand scan("newsletter", "Notifies you when a new message appears in the given channel.", bot.me.id);
+            dpp::slashcommand list("list", "Lists all your subscribed channels.", bot.me.id);
+            dpp::slashcommand subscribe("subscribe", "Notifies you when a new message appears in the given channel.", bot.me.id);
+            dpp::slashcommand unsubscribe("unsubscribe", "unsubscribes you from a newsletter for a given channel.", bot.me.id);
 
-            dpp::slashcommand count("profile", "counts the reactions of the calling user and returns the results", bot.me.id);
-            count.add_option(
-                dpp::command_option(dpp::co_string, "level", "the level of the search for messages. "
-//                                                             "if the level is local, only the current channel is searched. "
-//                                                             "if the level is guild, then all channels in the current guild are being searched. *1 ",
-//                                                             "if the level is global, then all guilds are being searched. *1 ",
-//                                                             "*1 Keep in mind, that only channels are scanned, which are indexed, aka channels in which /scan has been called.",
-                                    , false)
-                    .add_choice(dpp::command_option_choice("local", std::string("local")))
-                    .add_choice(dpp::command_option_choice("guild", std::string("guild")))
-                    .add_choice(dpp::command_option_choice("global", std::string("global")))
+            subscribe.add_option(
+                dpp::command_option(dpp::co_channel, "channel_id", "Id of the channel you want to watch.", true)
+            );
+            unsubscribe.add_option(
+                dpp::command_option(dpp::co_channel, "channel_id", "Id of the channel you want to watch.", true)
             );
 
-            bot.global_command_create(scan);
-            bot.global_command_create(count);
+            bot.global_command_create(list);
+            bot.global_command_create(subscribe);
+            bot.global_command_create(unsubscribe);
         }
     });
   
