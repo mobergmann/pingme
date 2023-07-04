21c77f7c6ac9f851ce0df20c0823fae825ec8152
mobergmann <mobergmann@pm.me>
2023-07-04 23:40:06 +0200

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,7 @@
 #include <string>
 #include <csignal>
 #include <fstream>
-#include <istream>
-#include <ostream>
+#include <filesystem>
 
 #include <dpp/dpp.h>
 #include <dpp/snowflake.h>
@@ -21,9 +20,14 @@ std::map<dpp::snowflake, std::set<dpp::snowflake>> newsletters;
 /// mutex for locking the newsletters global variable
 std::mutex lock;
 
-const std::string newsletters_file_path = "newsletters.json";
+const std::filesystem::path newsletters_file_path = "newsletters.json";
 
 void deserialize() {
+    if (not std::filesystem::exists(newsletters_file_path)) {
+        std::cerr << "the file '" << newsletters_file_path << "' could not be found. this means, that the bot will start with empty subscriptions" << std::endl;
+        return;
+    }
+
     lock.lock();
 
     // load file into json
@@ -37,23 +41,154 @@ void deserialize() {
     lock.unlock();
 }
 
-void serialize(int signal) {
+void serialize() {
     lock.lock();
     
     // convert newsletter to json
-    json j = newsletters;
+    json j(newsletters);
     
     // write json to file
     std::ofstream o(newsletters_file_path);
+    o << std::setw(4) << j << std::endl;
 
     lock.unlock();
 }
 
+void serialize_signal(int signal = 0) {
+    serialize();
+}
+
+void list(const dpp::slashcommand_t &event) {
+    const auto& channels = newsletters[event.command.usr.id];
+    if (channels.empty()) {
+        event.reply("You have no active subscriptions.");
+    }
+    else {
+        std::string reply = "You subscribed for the newsletters of the following channels: ";
+        for (const auto& channel_id: channels) {
+            const auto channel = dpp::find_channel(channel_id);
+            if (not channel) {
+                newsletters[event.command.usr.id].erase(channel_id);
+                reply += "\n- This channel does not exist anymore. It will be removed from the list.";
+            } else {
+                // todo: if channel is category
+                reply += "\n- " + channel->get_mention();
+            }
+        }
+        event.reply(reply);
+    }
+    serialize();
+}
+
+void subscribe(dpp::cluster &bot, const dpp::slashcommand_t &event) {
+    lock.lock();
+
+    dpp::snowflake channel_id = std::get<dpp::snowflake>(event.get_parameter("channel"));
+    const auto channel = dpp::find_channel(channel_id);
+    // todo only continue if bot has access to channel
+    if (not channel) {
+        lock.unlock();
+        event.reply("The channel you provided does not exist.");
+        return;
+    }
+
+    // test if the bot can send a dm to the user
+    dpp::message m(fmt::format("I will send you the newsletter for the channel {} you subscribed to per DM.", channel->get_mention()));
+    bot.direct_message_create(event.command.usr.id, m, [event, channel, channel_id](const auto& e){
+        if (e.is_error()) {
+            lock.unlock();
+            event.reply("It seems, that I couldn't send you a DM. Please enable the server setting 'Privacy Settings'->'Direct Messages=ON'");
+            return;
+        }
+
+        newsletters[event.command.usr.id].insert(channel_id);
+        event.reply(fmt::format("You subscribed for the newsletters for the channel {}", channel->get_mention()));
+
+        lock.unlock();
+    });
+}
+
+void unsubscribe(const dpp::slashcommand_t &event) {
+    lock.lock();
+
+    dpp::snowflake channel_id = std::get<dpp::snowflake>(event.get_parameter("channel"));
+
+    newsletters[event.command.usr.id].erase(channel_id);
+
+    const auto channel = dpp::find_channel(channel_id);
+    if (not channel) {
+        event.reply("The channel you provided does not exist.");
+    } else {
+        event.reply(fmt::format("You have successfully removed the channel {} from your newsletters.", channel->get_mention()));
+    }
+
+    lock.unlock();
+
+    serialize();
+}
+
+void handle_message(dpp::cluster &bot, const dpp::message_create_t &event) {
+    const auto origin_channel_id = event.msg.channel_id;
+    const auto origin_author_id = event.msg.author.id;
+
+    lock.lock();
+
+    for (const auto& [subscriber_id, channels]: newsletters) {
+        // if sender is the same as the iterator, then skip
+        if (origin_author_id == subscriber_id or origin_author_id == bot.me.id) {
+            lock.unlock();
+            return;
+        }
+
+        // check if user exists
+        const auto& subscriber = dpp::find_user(subscriber_id);
+        if (not subscriber) {
+            // delete user which does not exit
+            newsletters.erase(subscriber_id);
+            lock.unlock();
+            return;
+        }
+
+        // if user has a newsletters
+        if (not channels.contains(origin_channel_id)) {
+            lock.unlock();
+            return;
+        }
+
+        // try to get channel
+        const auto channel = dpp::find_channel(origin_channel_id);
+        if (not channel) {
+            // delete channel which does not exit
+            newsletters[subscriber_id].erase(origin_channel_id);
+            lock.unlock();
+            return;
+        }
+
+        // ping user, that a new message was received
+        dpp::message m(fmt::format("A new message in the channel {} was send. Go check it out.", channel->get_mention()));
+        bot.direct_message_create(subscriber_id, m, [event](const auto& e){
+            /*if (e.is_error()) {
+                event.reply("It seems, that I couldn't send you a DM. Please enable the server setting 'Privacy Settings'->'Direct Messages=ON'");
+                return;
+            }*/
+            // todo: what to do when we cannot ping a user anymore (maybe, because he/she turned of the privacy feature)?
+            lock.unlock();
+
+            serialize();
+        });
+    }
+}
 
 int main(int argc, char **argv)
 {
-    // when a system interrupt occurred serialize the newsletters
-    std::signal(SIGINT, &serialize);
+    /*
+    // when an interrupt signal occurs serialize the newsletters
+    std::signal(SIGINT, &serialize_signal);
+    // when a terminate signal occurs serialize the newsletters
+    std::signal(SIGTERM, &serialize_signal);
+    */
+
+    // initially deserialize the json file to resume the last session
     deserialize();
 
     const std::string BOT_TOKEN = std::string(std::getenv("BOT_TOKEN"));
@@ -63,98 +198,18 @@ int main(int argc, char **argv)
     bot.on_log(dpp::utility::cout_logger());
 
     bot.on_message_create([&](const dpp::message_create_t &event) {
-        const auto origin_channel_id = event.msg.channel_id;
-        const auto origin_author_id = event.msg.author.id;
-
-        for (const auto& [subscriber_id, channels]: newsletters) {
-            // if sender is the same as the iterator, then skip
-            if (origin_author_id == subscriber_id or origin_author_id == bot.me.id) {
-                return;
-            }
-
-            // check if user exists
-            const auto& subscriber = dpp::find_user(subscriber_id);
-            if (not subscriber) {
-                // delete user which does not exit
-                newsletters.erase(subscriber_id);
-                return;
-            }
-
-            // if user has a newsletters
-            if (not channels.contains(origin_channel_id)) {
-                return;
-            }
-
-            // try to get channel
-            const auto channel = dpp::find_channel(origin_channel_id);
-            if (not channel) {
-                // delete channel which does not exit
-                newsletters[subscriber_id].erase(origin_channel_id);
-                return;
-            }
-
-            // ping user, that a new message was received
-            dpp::message m(fmt::format("A new message in the channel {} was send. Go check it out.", channel->get_mention()));
-            bot.direct_message_create(subscriber_id, m, [event](const auto& e){
-                /*if (e.is_error()) {
-                    event.reply("It seems, that I couldn't send you a DM. Please enable the server setting 'Privacy Settings'->'Direct Messages=ON'");
-                    return;
-                }*/
-                // todo: what to do when we cannot ping a user anymore (maybe, because he/she turned of the privacy feature)?
-            });
-        }
+        handle_message(bot, event);
     });
 
     bot.on_slashcommand([&bot](const dpp::slashcommand_t &event) {
         if (event.command.get_command_name() == "list") {
-            const auto& channels = newsletters[event.command.usr.id];
-            if (channels.empty()) {
-                event.reply("You have no active subscriptions.");
-            }
-            else {
-                std::string reply = "You subscribed for the newsletters of the following channels: ";
-                for (const auto& channel_id: channels) {
-                    const auto channel = dpp::find_channel(channel_id);
-                    if (not channel) {
-                        newsletters[event.command.usr.id].erase(channel_id);
-                        reply += "\n- This channel does not exist anymore. It will be removed from the list.";
-                    } else {
-                        // todo: if channel is category
-                        reply += "\n- " + channel->get_mention();
-                    }
-                }
-                event.reply(reply);
-            }
+            list(event);
         }
         else if (event.command.get_command_name() == "subscribe") {
-            // test if the bot can send a dm to the user
-            dpp::message m("I will send you the newsletter for the channels you subscribed to per DM.");
-            bot.direct_message_create(event.command.usr.id, m, [event](const auto& e){
-                if (e.is_error()) {
-                    event.reply("It seems, that I couldn't send you a DM. Please enable the server setting 'Privacy Settings'->'Direct Messages=ON'");
-                    return;
-                }
-
-                dpp::snowflake channel_id = std::get<dpp::snowflake>(event.get_parameter("channel"));
-                const auto channel = dpp::find_channel(channel_id);
-                // todo only continue if bot has access to channel
-                if (not channel) {
-                    event.reply("The channel you provided does not exist.");
-                } else {
-                    newsletters[event.command.usr.id].insert(channel_id);
-                    event.reply(fmt::format("You subscribed for the newsletters for the channel {}", channel->get_mention()));
-                }
-            });
+            subscribe(bot, event);
         }
         else if (event.command.get_command_name() == "unsubscribe") {
-            dpp::snowflake channel_id = std::get<dpp::snowflake>(event.get_parameter("channel"));
-            newsletters[event.command.usr.id].erase(channel_id);
-            const auto channel = dpp::find_channel(channel_id);
-            if (not channel) {
-                event.reply("The channel you provided does not exist.");
-            } else {
-                event.reply(fmt::format("You have successfully removed the channel {} from your newsletters.", channel->get_mention()));
-            }
+            unsubscribe(event);
         }
     });
 
